18fe5ffc0695c188e152d13327c2edc0f521716b
Jean Choe <jeannchoee@gmail.com>
2023-11-25 15:34:23 -0800

    First commit

---
+++ funcs.ts
@@ -0,0 +1,39 @@
+/** Calculates (incorrectly) the value (n-1)^2. */
+export const quadratic3 = (n: number): number => {
+  return 0;  // TODO: replace this
+}
+
+/** Calculates (incorrectly) the value |x|.  */
+export const abs_value3 = (x: number): number => {
+  return 0;  // TODO: replace this
+}
+
+/** Calculates (incorrectly) the value |x|.  */
+export const abs_value4 = (x: number): number|undefined => {
+  return 0;  // TODO: replace this
+}
+
+/**
+ * Returns the number of pairs we can get from n items, where n is a
+ * non-negative integer.
+ */
+export const count_pairs = (n: number): number => {
+  return 0;  // TODO: replace this
+}
+
+/**
+ * Returns the number of pairs we can get from n items, where n is a
+ * non-negative integer.
+ */
+export const count_pairs2 = (n: number): number => {
+  return 0;  // TODO: replace this
+}
+
+
+// TODO: add the definition of "u" here
+
+
+// TODO: add the definition of "v" here
+
+
+// TODO: add the definition of "w" here
